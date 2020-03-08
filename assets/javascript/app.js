2af3c3013dbd2f35eb388bd55483cea8efe211a6
claire-labry <clairestjlabry@gmail.com>
2020-03-07 19:06:58 -0600

    cleaning up some bug fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 // Event Listener
-$("button").on("click", function(){
+$("div.container").on("click", "button[data-mood]", function(){
+// this will now work because the div with a class of container will always exist. Anyway I'm going to collapse now lol
 
+// $("button[data-mood]").on("click", function(){
+// one last thing (before my laptop dies). This listener will NOT work for buttons that haven't been created at page load. The reason is listeners only get attached once. The way we fix this is by targetting a parent element of the elements we want to target, and then delegating to the elements we want to click (since the parent element will be expected to always exist on the page). So the way we fix it is by keeping the selector we have here, but moving it
 // Inital Array of Moods
 
 var moods =  ["Excited", "Calm", "Perplexed", "Mind-Blown", "Livid"];
@@ -17,6 +20,8 @@ $.ajax({
 .then(function(response){
     console.log(response);
      var results = response.data;
+     $("#gifs-appear-here").empty();   
+
 
      for (var i = 0; i < results.length; i++){
          if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
@@ -32,6 +37,35 @@ $.ajax({
 
              $("#gifs-appear-here").prepend(gifDiv)
            }
-         }
+        }
+
+    moodImage.attr("src", results[i].images.fixed_height_still.url);
+    moodImage.attr({"data-animate": results[i].images.fixed_height.url});
+    moodImage.attr({"data-state": "still"});
+    moodImage.attr({"data-still": results[i].images.fixed_height_still.url});
+
+
+    var states = $(this).attr("data-state");
+
+    $('<div>').click(function(){
+        var index = parseInt($(this).attr("data-state"));
+        var state = states[index];
+
+        $('<div>').text(state);
+        if (state === "still"){
+            $(this).attr("src", $(this).attr("gif-animate"));
+            $(this).attr("data-state", "animate");
+        } else{
+           $(this).attr("src", $(this).attr("gif-still"));
+           $(this).attr("data-state", "still");
+        }
+    
     });
+});
+
+
+$('#add-mood').click(function(event) {
+    event.preventDefault();
+    // this stops the page from reloading (because you are using a button of type submit)
+});
 });
